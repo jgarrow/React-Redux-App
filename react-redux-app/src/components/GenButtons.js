3d11bbe5f155ede4c2c41ddcc089b59c1eda1b2b
Janessa Garrow <janessa.garrow@gmail.com>
2020-02-06 14:38:20 -0700

    Add styling to generation buttons

--- react-redux-app/src/components/GenButtons.js
+++ react-redux-app/src/components/GenButtons.js
@@ -4,35 +4,61 @@ import styled from "styled-components";
 
 import { getPokemon } from "../actions";
 
+const ButtonContainer = styled.div`
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-evenly;
+    align-items: center;
+    width: 100%;
+`;
+
+const Button = styled.p`
+    border-radius: 50%;
+    cursor: pointer;
+    background: #81a4db;
+    height: 73px;
+    width: 73px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
+
+    &:hover {
+        color: white;
+        background: #356abc;
+        box-shadow: 0px 0px 10px 5px rgba(0, 0, 0, 0.25);
+    }
+`;
+
 const GenButtons = props => {
     const handleGetNewGen = apiUrl => {
         props.getPokemon(apiUrl);
     };
 
     return (
-        <div>
-            <p onClick={() => handleGetNewGen(props.genApiUrls["gen1"])}>
+        <ButtonContainer>
+            <Button onClick={() => handleGetNewGen(props.genApiUrls["gen1"])}>
                 Gen I
-            </p>
-            <p onClick={() => handleGetNewGen(props.genApiUrls["gen2"])}>
+            </Button>
+            <Button onClick={() => handleGetNewGen(props.genApiUrls["gen2"])}>
                 Gen II
-            </p>
-            <p onClick={() => handleGetNewGen(props.genApiUrls["gen3"])}>
+            </Button>
+            <Button onClick={() => handleGetNewGen(props.genApiUrls["gen3"])}>
                 Gen III
-            </p>
-            <p onClick={() => handleGetNewGen(props.genApiUrls["gen4"])}>
+            </Button>
+            <Button onClick={() => handleGetNewGen(props.genApiUrls["gen4"])}>
                 Gen IV
-            </p>
-            <p onClick={() => handleGetNewGen(props.genApiUrls["gen5"])}>
+            </Button>
+            <Button onClick={() => handleGetNewGen(props.genApiUrls["gen5"])}>
                 Gen V
-            </p>
-            <p onClick={() => handleGetNewGen(props.genApiUrls["gen6"])}>
+            </Button>
+            <Button onClick={() => handleGetNewGen(props.genApiUrls["gen6"])}>
                 Gen VI
-            </p>
-            <p onClick={() => handleGetNewGen(props.genApiUrls["gen7"])}>
+            </Button>
+            <Button onClick={() => handleGetNewGen(props.genApiUrls["gen7"])}>
                 Gen VII
-            </p>
-        </div>
+            </Button>
+        </ButtonContainer>
     );
 };
 
