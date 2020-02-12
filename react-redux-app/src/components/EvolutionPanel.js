d3ba7fc95993d990f97228bb45c5515b86b9e02b
Janessa Garrow <janessa.garrow@gmail.com>
2020-02-11 20:29:21 -0700

    Add functioning slides for tier II evolution sprites

--- react-redux-app/src/components/EvolutionPanel.js
+++ react-redux-app/src/components/EvolutionPanel.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { connect } from "react-redux";
 
 import { PanelRow, Screen } from "./StyledComponents";
-import { Sprite } from "./LeftPanel";
+
+import { IoMdArrowDropupCircle, IoMdArrowDropdownCircle } from "react-icons/io";
 
 const EvolPanel = styled(PanelRow)`
     flex-wrap: wrap;
@@ -32,13 +33,88 @@ const EvolutionName = styled(Screen)`
     text-align: right;
 `;
 
-const SmallSprite = styled(Sprite)`
+const SpriteScreen = styled.div`
+    width: 100%;
+    position: relative;
+    overflow: hidden;
+    min-height: 120px;
+    max-height: 120px;
+    border: inset #9aa28b 3px;
+    border-radius: 5px;
+    box-sizing: border-box;
+    background: linear-gradient(
+        15deg,
+        #cad5b5 64%,
+        #dde2d4 70%,
+        #dde2d4 81%,
+        #fff 86%,
+        #dde2d4 89%,
+        #dde2d4 100%
+    );
+`;
+
+const UpArrowIcon = styled(IoMdArrowDropupCircle)`
+    cursor: pointer;
+    position: absolute;
+    top: 2px;
+    left: 50px;
+    z-index: 5;
+`;
+
+const DownArrowIcon = styled(IoMdArrowDropdownCircle)`
+    cursor: pointer;
+    position: absolute;
+    bottom: 0;
+    left: 50px;
+    z-index: 5;
+`;
+
+const SpriteSlides = styled.div`
+    position: relative;
+    width: 100%;
+    height: 100%;
+    min-height: 120px;
+    max-height: 120px;
+    display: grid;
+    grid-template-rows: ${props => `repeat(${props.numOfSprites}, 100%)`};
+    transform: ${props => `translateY(${props.translateValue}%)`};
+    transition: transform 0.45s ease-out;
+`;
+
+const SmallSprite = styled.img`
     height: 120px;
     width: 120px;
     margin: 3px 0;
+    background: transparent;
+    image-rendering: pixelated;
+    display: flex;
 `;
 
 const EvolutionPanel = props => {
+    const [spriteIIEntryPosition, setSpriteIIEntryPosition] = useState(0);
+
+    const handleTransition = (evolTier, direction) => {
+        let newPosition = null;
+        let entryPositionState = null;
+
+        console.log("in handleTransition");
+
+        if (evolTier === "II") {
+            newPosition = spriteIIEntryPosition;
+            entryPositionState = spriteIIEntryPosition;
+        }
+
+        if (direction === "down") {
+            newPosition -= 100;
+        } else {
+            newPosition += 100;
+        }
+
+        if (entryPositionState === spriteIIEntryPosition) {
+            setSpriteIIEntryPosition(newPosition);
+        }
+    };
+
     return (
         <EvolPanel>
             <div>
@@ -63,18 +139,41 @@ const EvolutionPanel = props => {
                 <FlexCenter>
                     <EvolutionNum>II</EvolutionNum>
                 </FlexCenter>
-                {props["evolution_line"]["evolution_II"] && (
-                    <>
-                        <SmallSprite
-                            src={props["evolution_sprites"]["evol_II"][0]}
-                            alt={props["evolution_line"]["evolution_II"][0]}
-                        />
-
-                        <EvolutionName>
-                            {props["evolution_line"]["evolution_II"][0]}
-                        </EvolutionName>
-                    </>
-                )}
+                <SpriteScreen>
+                    {props["evolution_line"]["evolution_II"] && (
+                        <>
+                            <UpArrowIcon
+                                onClick={() => handleTransition("II", "up")}
+                            />
+                            <SpriteSlides
+                                translateValue={spriteIIEntryPosition}
+                                numOfSprites={
+                                    props["evolution_sprites"]["evol_II"].length
+                                }
+                            >
+                                {props["evolution_sprites"]["evol_II"].map(
+                                    (sprite, index) => (
+                                        <SmallSprite
+                                            key={index}
+                                            src={sprite}
+                                            alt={
+                                                props["evolution_line"][
+                                                    "evolution_II"
+                                                ][index]
+                                            }
+                                        />
+                                    )
+                                )}
+                            </SpriteSlides>
+                            <DownArrowIcon
+                                onClick={() => handleTransition("II", "down")}
+                            />
+                            <EvolutionName>
+                                {props["evolution_line"]["evolution_II"][0]}
+                            </EvolutionName>
+                        </>
+                    )}
+                </SpriteScreen>
             </div>
 
             <div>
